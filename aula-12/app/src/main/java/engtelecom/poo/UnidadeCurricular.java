4cc91406d69aad71426d0ec14fa77cad3851b6ca
luiza <luizakuze08@gmail.com>
2023-09-12 11:25:03 -0300

    exercicio unidade curricular

---
+++ aula-12/app/src/main/java/engtelecom/poo/UnidadeCurricular.java
@@ -0,0 +1,25 @@
+package engtelecom.poo;
+
+import java.util.ArrayList;
+
+public class UnidadeCurricular {
+    
+    private String nome;
+    private String codigo;
+    private String sigla;
+    private Integer creditos;
+    private String ementa;
+    private ArrayList<String> objetivo; //1 a n objetivos
+    private ArrayList<String> conteudo;
+
+    // decidir 20*4 = 80; 4 creditos sao 4 aulas p semana; 1 encontro = 2 creditos
+    //private Integer semanas;
+    // private Integer cargaHoraria; calculo
+    //getCH(int semana)
+
+    // sem getter e setter p objetivo e conteudo
+    // no construtor sem objetivo e conteudo
+
+
+
+}

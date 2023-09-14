7497231a096b6c21878f796bf2d24ea2ebc4f112
luiza <luizakuze08@gmail.com>
2023-09-14 16:26:11 -0300

    organizando por aula

--- aula-12/app/src/main/java/engtelecom/poo/Curso.java
+++
@@ -1,9 +0,0 @@
-package engtelecom.poo;
-
-public class Curso {
-
-    // todas as disciplinas com 18 semanas
-    // static: nao precisa criar uma instância 
-    public static final int SEMANAS = 18;
-
-}

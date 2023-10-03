df2a842f94a34bb205cae494a1fef8fded618caf
luizakuze <luizakuze08@gmail.com>
2023-10-03 11:24:08 -0300

    add atributos

---
+++ aula-16/app/src/main/java/engtelecom/poo/Pessoa.java
@@ -0,0 +1,11 @@
+package engtelecom.poo;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNasc;
+    private Telefone telefones;
+    private Email emails;
+}

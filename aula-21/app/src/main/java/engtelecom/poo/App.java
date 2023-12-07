aac334ad6e14d2c451c85a86b8baf0e1484cba09
--global <--global>
2023-12-07 07:44:35 -0300

    a

---
+++ aula-21/app/src/main/java/engtelecom/poo/App.java
@@ -0,0 +1,12 @@
+package engtelecom.poo;
+
+
+public class App {
+
+    public void makeMovie(int num_frames) {
+        ABunchOfSpheres movie = new ABunchOfSphere();
+    }
+
+    public static void main(String[] args) {
+    }
+}
